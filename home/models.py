ead4fabd9d8b2e75db5c222aaafc15be39e0c6d5
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-05-17 18:27:58 +0200

    ggg

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, Group
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 from . import enums
